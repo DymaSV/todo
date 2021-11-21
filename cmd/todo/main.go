e5aa727320111d5fa1ed118325dfc319181e75d0
DymaSv <dymasvyrydonov@gmail.com>
2021-11-21 15:00:43 -0500

    Created simple write to file

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 	"strings"
 
-	"github.com/DymaSV/todo/proto"
+	"github.com/golang/protobuf/proto"
+
+	todo "github.com/DymaSV/todo/proto/todo"
 )
 
 func main() {
@@ -32,15 +38,65 @@ func main() {
 
 const dbPath = "mydb.pb"
 
-func add(test string) error {
-	task := &proto.Task{
+func add(text string) error {
+	task := &todo.Task{
 		Text: text,
 		Done: false,
 	}
-	proto.
+
+	b, err := proto.Marshal(task)
+	if err != nil {
+		fmt.Errorf("could not encode task: %v", err)
+	}
+
+	f, err := os.OpenFile(dbPath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Errorf("could not open file %s: %v", dbPath, err)
+	}
+
+	if err := gob.NewEncoder(f).Encode(int64(len(b))); err != nil {
+		fmt.Errorf("could not encode length of message: %v", err)
+	}
+
+	_, err = f.Write(b)
+	if err != nil {
+		fmt.Errorf("could not write task to file: %v", err)
+	}
+
+	if err := f.Close(); err != nil {
+		fmt.Errorf("could not close file %s: %v", dbPath, err)
+	}
 	return nil
 }
 
 func list() error {
-	return nil
+	b, err := ioutil.ReadFile(dbPath)
+	if err != nil {
+		return fmt.Errorf("could not read %s: %v", dbPath, err)
+	}
+	for {
+		if len(b) == 0 {
+			return nil
+		} else if len(b) < 4 {
+			return fmt.Errorf("remaining odd %d bytes, what to do?", err)
+		}
+
+		var length int64
+		if err = gob.NewDecoder(bytes.NewReader(b[:4])).Decode(&length); err != nil {
+			return fmt.Errorf("could not decode message length: %v", err)
+		}
+		b = b[4:]
+		var task todo.Task
+		if err := proto.Unmarshal(b[:length], &task); err == io.EOF {
+			return fmt.Errorf("could not read task: %v", err)
+		}
+		b = b[length:]
+
+		if task.Done {
+			fmt.Printf("OK")
+		} else {
+			fmt.Printf("Oups...")
+		}
+		fmt.Printf(" %s\n", task.Text)
+	}
 }
